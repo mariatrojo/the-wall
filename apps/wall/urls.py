2751e2d182d721549524c9fbb09381c0138a8a88
mariarojo <mariatrojo@gmail.com>
2018-06-25 17:10:46 -0700

    adding signin and register views

--- apps/wall/urls.py
+++ apps/wall/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index)
+	url(r'^$', views.index),
+	url(r'^signin$', views.signin),
+	url(r'^signin_form$', views.signin_form),
+	url(r'^register$', views.register),
+	url(r'^register_form$', views.register_form)
 ]
--- apps/wall/views.py
+++ apps/wall/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+from models import * 
 
 def index(request):
 	return render(request, "wall/index.html")
+
+def signin(request):
+	return render(request, "wall/signin.html")
+
+def register(request):
+	return render(request, "wall/register.html")
+
+def signin_form(request):
+	result = User.objects.login_validator(request.POST)
+	if type(result) == list:
+		for error in result:
+			messages.error(request, error)
+		return redirect('/signin')
+	else:
+		if result.user_level == 1:
+			request.session['user_id'] = result.id
+			return redirect('/dashboard')
+		else:
+			request.session['user_id'] = result.id
+			return redirect('/dashboard/admin')
+
+def register_form(request):
+	result = User.objects.register_validator(request.POST)
+	if type(result) == list:
+		for error in result:
+			messages.error(request, error)
+		return redirect('/register')
+	else:
+		if result.user_level == 1:
+			request.session['user_id'] = result.id
+			return redirect('/dashboard')
+		else:
+			request.session['user_id'] = result.id
+			return redirect('/dashboard/admin')
